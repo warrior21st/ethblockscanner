1d842542886deb94974b4fea95e09290e7753b91
warrior21st <123767168@qq.com>
2021-05-09 20:27:05 +0800

    add txlog_scanner

---
+++ simple_txlogwatcher.go
@@ -0,0 +1,86 @@
+package ethtxscanner
+
+import (
+	"encoding/base64"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+//简单交易管理结构
+type SimpleTxLogWatcher struct {
+	endpoints      []string
+	infuraSecrets  []string
+	scanStartBlock uint64
+	interestedLogs map[string]interface{}
+	scanInterval   time.Duration
+	callback       func(*types.Log) error
+}
+
+//构造一个新的简单tx管理结构(默认3秒钟扫描一次)
+func NewSimpleTxWatcher(endpoints []string, scanStartBlock uint64, scanInterval time.Duration, callback func(*types.Log) error) *SimpleTxLogWatcher {
+
+	return &SimpleTxLogWatcher{
+		endpoints:      endpoints,
+		scanStartBlock: scanStartBlock,
+		scanInterval:   scanInterval,
+		callback:       callback,
+	}
+}
+
+func (watcher *SimpleTxLogWatcher) SetInfuraSecrets(secrets []string) {
+	watcher.infuraSecrets = secrets
+}
+
+//添加关注的from address
+func (watcher *SimpleTxLogWatcher) AddInterestedLog(address string, topic0 string) {
+	if watcher.interestedLogs == nil {
+		watcher.interestedLogs = make(map[string]interface{})
+	}
+	watcher.interestedLogs[strings.ToLower(address+"_"+topic0)] = true
+}
+
+func (watcher *SimpleTxLogWatcher) GetScanStartBlock() uint64 {
+
+	return watcher.scanStartBlock
+}
+
+func (watcher *SimpleTxLogWatcher) GetEthClients() ([]*ethclient.Client, error) {
+	clients := make([]*ethclient.Client, len(watcher.endpoints))
+	for i := 0; i < len(watcher.endpoints); i++ {
+		rpcClient, err := rpc.Dial(watcher.endpoints[i])
+		if err != nil {
+			return nil, err
+		}
+		if i < len(watcher.infuraSecrets) && strings.Trim(watcher.infuraSecrets[i], " ") != "" {
+			rpcClient.SetHeader("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(":"+watcher.infuraSecrets[i])))
+		}
+
+		clients[i] = ethclient.NewClient(rpcClient)
+	}
+
+	return clients, nil
+}
+
+func (watcher *SimpleTxLogWatcher) IsInterestedTx(addr string, topic0 string) bool {
+
+	return watcher.interestedLogs[strings.ToLower(addr+"_"+topic0)] != nil
+}
+
+//tx回调处理方法
+func (watcher *SimpleTxLogWatcher) Callback(tx *types.Log) error {
+	return watcher.callback(tx)
+}
+
+//获取区块扫描间隔
+func (watcher *SimpleTxLogWatcher) GetScanInterval() time.Duration {
+	return watcher.scanInterval
+}
+
+//设置区块扫描间隔
+func (watcher *SimpleTxLogWatcher) SetScanInterval(interval time.Duration) {
+	watcher.scanInterval = interval
+}
--- txlog_scanner.go
+++ txlog_scanner.go
@@ -1,139 +1,149 @@
 package ethtxscanner
 
-// import (
-// 	"context"
-// 	"math/big"
-// 	"strconv"
-// 	"time"
+import (
+	"context"
+	"math/big"
+	"strconv"
+	"time"
 
-// 	"github.com/ethereum/go-ethereum/core/types"
-// 	"github.com/ethereum/go-ethereum/eth/filters"
-// 	"github.com/ethereum/go-ethereum/ethclient"
-// )
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/eth/filters"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
 
-// var (
-// 	_txlogWatcher          TxlogWatcher
-// 	_lastScanedBlockNumber uint64 = 0
-// 	_chainID               *big.Int
-// 	_signer                types.EIP155Signer
-// 	_clientSleepTimes      map[int]uint64
-// 	_filters               map[int]*filters.Filter
-// )
+var (
+	_txlogWatcher          TxlogWatcher
+	_lastScanedBlockNumber uint64 = 0
+	_chainID               *big.Int
+	_signer                types.EIP155Signer
+	_clientSleepTimes      map[int]uint64
+	_filters               map[int]*filters.Filter
+)
 
-// type TxlogWatcher interface {
-// 	//获取开始扫描的区块号
-// 	GetScanStartBlock() uint64
+type TxlogWatcher interface {
+	//获取开始扫描的区块号
+	GetScanStartBlock() uint64
 
-// 	//获取节点地址
-// 	GetEthClients() ([]*ethclient.Client, error)
+	//获取节点地址
+	GetEthClients() ([]*ethclient.Client, error)
 
-// 	//是否是需要解析的tx
-// 	GetLogFilterParams() []interface{}
+	//是否是需要解析的tx
+	IsInterestedLog(address string, topic0 string) bool
 
-// 	//tx log回调处理方法
-// 	Callback(txlogs []*types.Log) error
+	//tx log回调处理方法
+	Callback(txlogs []*types.Log) error
 
-// 	//获取扫描间隔
-// 	GetScanInterval() time.Duration
-// }
+	//获取扫描间隔
+	GetScanInterval() time.Duration
+}
 
-// //开始扫描
-// func StartScanTxLogs(txlogWatcher TxlogWatcher) error {
-// 	LogToConsole("eth tx scanner starting...")
-// 	_txlogWatcher = txlogWatcher
-// 	startBlock := _txlogWatcher.GetScanStartBlock()
-// 	if _lastScanedBlockNumber == 0 {
-// 		if startBlock > 0 {
-// 			_lastScanedBlockNumber = startBlock - 1
-// 		}
-// 	}
-// 	clients, err := _txlogWatcher.GetEthClients()
-// 	if err != nil {
-// 		return err
-// 	}
+//开始扫描
+func StartScanTxLogs(txlogWatcher TxlogWatcher) error {
+	LogToConsole("eth tx log scanner starting...")
+	_txlogWatcher = txlogWatcher
+	startBlock := _txlogWatcher.GetScanStartBlock()
+	if _lastScanedBlockNumber == 0 {
+		if startBlock > 0 {
+			_lastScanedBlockNumber = startBlock - 1
+		}
+	}
+	clients, err := _txlogWatcher.GetEthClients()
+	if err != nil {
+		return err
+	}
 
-// 	cid, err := clients[0].ChainID(context.Background())
-// 	if err != nil {
-// 		return err
-// 	}
-// 	_chainID = cid
-// 	_signer = types.NewEIP155Signer(_chainID)
-// 	LogToConsole("chainID:" + _chainID.String() + ",filter scaning...")
+	cid, err := clients[0].ChainID(context.Background())
+	if err != nil {
+		return err
+	}
+	_chainID = cid
+	_signer = types.NewEIP155Signer(_chainID)
+	LogToConsole("chainID:" + _chainID.String() + ",filter scaning...")
 
-// 	for i := 0; i < len(clients); i++ {
-// 		clients[i].Close()
-// 	}
+	for i := 0; i < len(clients); i++ {
+		clients[i].Close()
+	}
 
-// 	scanInterval := _txlogWatcher.GetScanInterval()
-// 	if scanInterval <= time.Millisecond {
-// 		scanInterval = 0
-// 	}
-// 	errCount := 0
-// 	for true {
-// 		scanedBlock, err := scanTxLogs(_lastScanedBlockNumber + 1)
-// 		if err != nil {
-// 			if scanedBlock > 0 {
-// 				_lastScanedBlockNumber = scanedBlock
-// 			} else {
-// 				errCount++
-// 			}
-// 		} else {
-// 			errCount = 0
-// 		}
+	scanInterval := _txlogWatcher.GetScanInterval()
+	if scanInterval <= time.Millisecond {
+		scanInterval = 0
+	}
+	errCount := 0
+	for true {
+		scanedBlock, err := scanTxLogs(_lastScanedBlockNumber + 1)
+		if err != nil {
+			if scanedBlock > 0 {
+				_lastScanedBlockNumber = scanedBlock
+			} else {
+				errCount++
+			}
+		} else {
+			errCount = 0
+		}
 
-// 		//如果连续报错达到10次，则线程睡眠10秒后继续
-// 		if errCount == 10 {
-// 			LogToConsole("scaning block continuous error " + strconv.Itoa(errCount) + " times,sleep 30s...")
-// 			time.Sleep(30 * time.Second)
-// 			errCount = 0
-// 		}
+		//如果连续报错达到10次，则线程睡眠10秒后继续
+		if errCount == 10 {
+			LogToConsole("scaning block continuous error " + strconv.Itoa(errCount) + " times,sleep 30s...")
+			time.Sleep(30 * time.Second)
+			errCount = 0
+		}
 
-// 		if scanInterval > 0 {
-// 			time.Sleep(scanInterval)
-// 		}
-// 	}
+		if scanInterval > 0 {
+			time.Sleep(scanInterval)
+		}
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
-// func scanTxLogs(startBlock uint64) (uint64, error) {
-// 	clients, err := _txlogWatcher.GetEthClients()
-// 	if err != nil {
-// 		return 0, err
-// 	}
+func scanTxLogs(startBlock uint64) (uint64, error) {
+	clients, err := _txlogWatcher.GetEthClients()
+	if err != nil {
+		return 0, err
+	}
 
-// 	for i := 0; i < len(clients); i++ {
-// 		defer clients[i].Close()
-// 	}
+	for i := 0; i < len(clients); i++ {
+		defer clients[i].Close()
+	}
 
-// 	errorSleepSeconds := 10
-// 	currBlock := startBlock
-// 	finisedMaxBlock := startBlock - 1
-// 	for true {
-// 		avaiIndexes := RebuildAvaiIndexes(len(clients), &_clientSleepTimes)
-// 		if len(avaiIndexes) == 0 {
-// 			break
-// 		}
-// 		for len(avaiIndexes) > 0 {
-// 			index := avaiIndexes[currBlock%uint64(len(avaiIndexes))]
-// 			client := clients[index]
-// 			LogToConsole("scaning block " + strconv.FormatUint(currBlock, 10) + "tx logs on client_" + strconv.Itoa(index, 10) + "...")
+	errorSleepSeconds := 10
+	currBlock := startBlock
+	finisedMaxBlock := startBlock - 1
+	filter := ethereum.FilterQuery{}
+	for true {
+		avaiIndexes := RebuildAvaiIndexes(len(clients), &_clientSleepTimes)
+		if len(avaiIndexes) == 0 {
+			break
+		}
+		for len(avaiIndexes) > 0 {
+			index := avaiIndexes[currBlock%uint64(len(avaiIndexes))]
+			client := clients[index]
+			LogToConsole("scaning block " + strconv.FormatUint(currBlock, 10) + "tx logs on client_" + strconv.Itoa(index, 10) + "...")
 
-// 			block, err := client.BlockByNumber(context.Background(), new(big.Int).SetUint64(currBlock))
-// 			if err != nil {
-// 				_clientSleepTimes[index] = time.Now().UTC().Unix() + 10
-// 				LogToConsole("client_" + strconv.Itoa(index, 10) + "response error,sleep " + strconv.Itoa(errorSleepSeconds) + "s.")
-// 				continue
-// 			}
+			filter.BlockHash = common.BytesToHash(new(big.Int).SetUint64(currBlock))
+			logs, err := client.FilterLogs(context.Background(), filter)
+			if err != nil {
+				_clientSleepTimes[index] = time.Now().UTC().Unix() + 10
+				LogToConsole("client_" + strconv.Itoa(index, 10) + "response error,sleep " + strconv.Itoa(errorSleepSeconds) + "s.")
+				continue
+			}
 
-// 			if block == nil {
-// 				break
-// 			}
+			if logs == nil {
+				break
+			}
 
-// 			finisedMaxBlock = currBlock
-// 			currBlock++
-// 		}
-// 	}
+			for _, log := range logs {
+				if _txlogWatcher.IsInterestedLog(log.Address.Hex(), log.Topics[0].Hex()) {
+					_txlogWatcher.Callback(log)
+				}
+			}
 
-// 	return finisedMaxBlock, nil
-// }
+			finisedMaxBlock = currBlock
+			currBlock++
+		}
+	}
+
+	return finisedMaxBlock, nil
+}
